6befb5b8be35e2e6f86aad1c40d2694acf7a3e63
AlexHuie <ahuie26@moundsparkacademy.org>
2025-01-20 17:01:44 -0600

    Why

--- Reefscape 2025/src/main/java/frc/robot/Robot.java
+++ Reefscape 2025/src/main/java/frc/robot/Robot.java
@@ -4,13 +4,19 @@
 
 package frc.robot;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class Robot extends TimedRobot {
 
   private final SparkMax motor = new SparkMax(15, MotorType.kBrushless);
+  private final CommandXboxController controller = new CommandXboxController(0);
 
   private Command m_autonomousCommand;
 
@@ -57,7 +63,9 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    motor.set(controller.getLeftY());
+  }
 
   @Override
   public void teleopExit() {}
