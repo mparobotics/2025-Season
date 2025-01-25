e6a2a9424530e52e24f50a00ae4ea3f8263981da
AlexHuie <ahuie26@moundsparkacademy.org>
2025-01-25 14:11:43 -0600

    Untested code finished

--- Reefscape 2025/src/main/java/frc/robot/RobotContainer.java
+++ Reefscape 2025/src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,27 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController.Axis;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Subsystems.ClimbSubsystem;
 
 public class RobotContainer {
+
+  private final ClimbSubsystem m_Climb = new ClimbSubsystem();
+
+  private final CommandXboxController controller = new CommandXboxController(0);
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    controller.axisGreaterThan(Axis.kLeftY.value, 0.1).whileTrue(m_Climb.changeArmSetpointCommand(-1).repeatedly());
+    controller.axisLessThan(Axis.kLeftY.value, -0.1).whileTrue(m_Climb.changeArmSetpointCommand(1).repeatedly());
+
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
--- Reefscape 2025/src/main/java/frc/robot/Subsystems/ClimbSubsystem.java
+++ Reefscape 2025/src/main/java/frc/robot/Subsystems/ClimbSubsystem.java
@@ -6,17 +6,27 @@ package frc.robot.Subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbSubsystem extends SubsystemBase {
   private final TalonFX climbMotor = new TalonFX(0);
   private final TalonFXConfiguration motorConfiguration = new TalonFXConfiguration();
 
+  private double m_setpoint = 0;
+
   /** Creates a new ClimbSubsystem. */
   public ClimbSubsystem() {
     climbMotor.getConfigurator().apply(motorConfiguration);
   }
+  public void setTarget(double degrees){
+    m_setpoint = degrees;
+  }
 
+  public Command changeArmSetpointCommand(double change){
+    return runOnce(() -> { setTarget(m_setpoint + change); });
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
