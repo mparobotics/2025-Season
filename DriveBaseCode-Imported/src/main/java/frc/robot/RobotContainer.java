7c8a66d7c4dfb9dae13ec0081356e65d5e688169
leobxrger <lstout28@moundsparkacademy.org>
2025-02-03 17:15:35 -0600

    updated a few things

--- DriveBaseCode-Imported/src/main/java/frc/robot/RobotContainer.java
+++ DriveBaseCode-Imported/src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
   private final CommandXboxController driveController = new CommandXboxController(0); 
