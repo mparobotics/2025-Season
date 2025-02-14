a107c046b2c402562d84489191e2107d1a2488ba
Lilly <113945482+raininglilly@users.noreply.github.com>
2025-02-14 17:24:56 -0600

    commit!

--- DriveBaseCode-Imported/src/main/java/frc/robot/Constants.java
+++ DriveBaseCode-Imported/src/main/java/frc/robot/Constants.java
@@ -19,7 +19,7 @@ import frc.lib.SwerveModuleConstants;
 /** Add your docs here. */
 public final class Constants {
   public final class ElevatorConstants{
-    public static final int elevatorMotorID = 15; //Placeholder, for test bot 
+    public static final int elevatorMotorID = 1; //Placeholder, for test bot 
   }
   public final class WristConstants{
     public static final int wristMotorID = 15; //Placehoder for actual motor ID
