ba241fc4cad749a8cd34bd80af73eafaa0c346a3
Seqi <gavin2noah@gmail.com>
2025-09-05 15:50:12 -0500

    Zero and Lib updates for club fair

--- ENTIRECODE/src/main/java/frc/robot/Constants.java
+++ ENTIRECODE/src/main/java/frc/robot/Constants.java
@@ -177,10 +177,10 @@ public static final double motorSpeedMultiplier = 0.5; // Used to scale down mot
     ){}
 
     public static ModuleData[] moduleData = {
-      new ModuleData(4, 2, 3, 98.79, FRONT_LEFT), //Mod 0
-      new ModuleData(7, 5, 6, -68.03, FRONT_RIGHT), //Mod 1
-      new ModuleData(10, 8, 9, 32.87, BACK_RIGHT), //Mod 2
-      new ModuleData(13, 11, 12, 162.77, BACK_LEFT) //Mod 3
+      new ModuleData(4, 2, 3, 97.82, FRONT_LEFT), //Mod 0
+      new ModuleData(7, 5, 6, 270.61, FRONT_RIGHT), //Mod 1
+      new ModuleData(10, 8, 9, 33.31, BACK_RIGHT), //Mod 2
+      new ModuleData(13, 11, 12, 213.49, BACK_LEFT) //Mod 3
     };
       
     public static final double angleKP = 0.01; //to tune
