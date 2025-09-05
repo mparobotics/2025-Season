3023ab2ae34d44b29702b5da40dc19f6175b9a9d
Seqi <gavin2noah@gmail.com>
2025-09-05 16:21:16 -0500

    one more change

--- ENTIRECODE/src/main/java/frc/robot/Robot.java
+++ ENTIRECODE/src/main/java/frc/robot/Robot.java
@@ -33,26 +33,7 @@ public class Robot extends TimedRobot {
   public void disabledExit() {}
 
   @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void autonomousExit() {}
-
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
+  public void teleopInit() {}
 
   @Override
   public void teleopPeriodic() {}
