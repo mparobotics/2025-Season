59bd2e769c28f827da870e83875674c316d76f8c
Nonochen0104 <xchen26@moundsparkacademy.org>
2025-11-22 11:14:48 -0600

    re-zeroed, decreased the speed

--- ENTIRECODE/src/main/java/frc/robot/RobotContainer.java
+++ ENTIRECODE/src/main/java/frc/robot/RobotContainer.java
@@ -88,8 +88,8 @@ public class RobotContainer {
     m_drive.setDefaultCommand(
     new TeleopSwerve(
         m_drive,
-        () -> -getSpeedMultiplier() * driveController.getRawAxis(translationAxis) * 0.75, // * for speed change drive
-        () -> -getSpeedMultiplier() * driveController.getRawAxis(strafeAxis) * 0.75, // * for speed change drive
+        () -> -getSpeedMultiplier() * driveController.getRawAxis(translationAxis) * 0.5, // * for speed change drive
+        () -> -getSpeedMultiplier() * driveController.getRawAxis(strafeAxis) * 0.5, // * for speed change drive
         () -> -driveController.getRawAxis(rotationAxis) * 0.75, //put - infront of drivecontroller or take it away to tune the turning // * for speed change turn
         () -> robotCentric.getAsBoolean(),
         () -> driveController.getRightTriggerAxis() > 0.1
